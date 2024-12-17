6e39c3e832e1bcca6462397d6256cb0bb8517aed
ihtisham-123 <161308347+ihtisham-123@users.noreply.github.com>
2024-12-17 19:04:21 +0500

    Update firebase.ts

--- hooks/firebase.ts
+++ hooks/firebase.ts
@@ -11,13 +11,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDrQSA5qA0py7hCDx5KZVMn3VFIdU9f0wM",
-  authDomain: "music-app-8c59a.firebaseapp.com",
-  projectId: "music-app-8c59a",
-  storageBucket: "music-app-8c59a.firebasestorage.app",
-  messagingSenderId: "227426745499",
-  appId: "1:227426745499:web:f079b24b4a8dac10845a3e",
-  measurementId: "G-LW5VYN7NNL"
+ 
 };
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH= getAuth(FIREBASE_APP);
